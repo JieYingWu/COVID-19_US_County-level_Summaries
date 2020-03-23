14c61a1cb7ef718acb035d259e26ad2d40491472
Benjamin Killeen <benjamindkilleen@gmail.com>
2020-03-23 14:20:20 -0500

    changes to loader

--- scripts/loader.py
+++ scripts/loader.py
@@ -1,11 +1,14 @@
+
+from os.path import join, exists
 from torch.utils.data import Dataset
 
 
 def Counties(Dataset):
-  def __init__(self, ):
+  def __init__(self, data_dir):
     pass
   
   def __getitem__(self, i):
     pass
 
-  def __len__(self, i)
+  def __len__(self, i):
+    pass
