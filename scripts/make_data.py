1fa6640cc02ad2e8d8de1bc7905ac3080156df10
Benjamin Killeen <benjamindkilleen@gmail.com>
2020-03-23 18:48:58 -0500

    added demographics data, but seems to be sparse

--- scripts/make_data.py
+++ scripts/make_data.py
@@ -77,8 +77,9 @@ class Formatter():
     'poverty',
     'unemployment',
     'climate',
-    'density'
-  ]             # TODO: add density
+    'density',
+    'demographics'
+  ]
   
   national_data_skiprows = {
     'population': 2,
@@ -86,7 +87,8 @@ class Formatter():
     'poverty': 4,
     'unemployment': 4,
     'climate': 0,
-    'density': 1}
+    'density': 1,
+    'demographics': 0}
 
   # which column has the fips code in each table
   fips_columns = {
@@ -95,7 +97,8 @@ class Formatter():
     'poverty': 0,
     'unemployment': 0,
     'climate': 0,
-    'density': 3}
+    'density': 3,
+    'demographics': 0}
   
   national_data_which_columns = OrderedDict([
     ('population', [
@@ -136,26 +139,26 @@ class Formatter():
     
     ('poverty', [
       7,                        # POVALL_2018
-      8,                      # CI90LBAll_2018
-      9,                      # CI90UBALL_2018
-      10,                      # PCTPOVALL_2018
-      11,                      # CI90LBALLP_2018
-      12,                      # CI90UBALLP_2018
-      13,                      # POV017_2018
-      14,                      # CI90LB017_2018
-      15,                      # CI90UB017_2018
-      16,                      # PCTPOV017_2018
-      17,                      # CI90LB017P_2018
-      18,                      # CI90UB017P_2018
-      19,                      # POV517_2018
-      20,                      # CI90LB517_2018
-      21,                      # CI90UB517_2018
-      22,                      # PCTPOV517_2018
-      23,                      # CI90LB517P_2018
-      24,                      # CI90UB517P_2018
-      25,                      # MEDHHINC_2018
-      26,                      # CI90LBINC_2018
-      27                       # CI90UBINC_2018
+      8,                        # CI90LBAll_2018
+      9,                        # CI90UBALL_2018
+      10,                       # PCTPOVALL_2018
+      11,                       # CI90LBALLP_2018
+      12,                       # CI90UBALLP_2018
+      13,                       # POV017_2018
+      14,                       # CI90LB017_2018
+      15,                       # CI90UB017_2018
+      16,                       # PCTPOV017_2018
+      17,                       # CI90LB017P_2018
+      18,                       # CI90UB017P_2018
+      19,                       # POV517_2018
+      20,                       # CI90LB517_2018
+      21,                       # CI90UB517_2018
+      22,                       # PCTPOV517_2018
+      23,                       # CI90LB517P_2018
+      24,                       # CI90UB517P_2018
+      25,                       # MEDHHINC_2018
+      26,                       # CI90LBINC_2018
+      27                        # CI90UBINC_2018
     ]),
     
     ('unemployment', [
@@ -225,6 +228,148 @@ class Formatter():
       11,                       # Area in square miles - Land area
       12,                       # Density per square mile of land area - Population
       13                        # Density per square mile of land area - Housing units
+    ]),
+    
+    ('demographics', [
+      3,                      #  Total_Male
+      4,                      #  Total_Female
+      5,                      # Total_age0to17
+      6,                      # Male_age0to17
+      7,                      # Female_age0to17
+      8,                      # Total_age18to64
+      9,                      # Male_age18to64
+      10,                      # Female_age18to64
+      11,                      # Total_age65plus
+      12,                      # Male_age65plus
+      13,                      # Female_age65plus
+      14,                      # Total_age85plusr
+      15,                      # Male_age85plusr
+      16,                      # Female_age85plusr
+      17,                      # pop_density
+      18,                      # Total households
+      19,                      # Total households!!Family households (families)
+      20,                      # Total households!!Family households (families)!!With own children of the householder under 18 years
+      21,                      # Total households!!Family households (families)!!Married-couple family
+      22,                      # Total households!!Family households (families)!!Married-couple family!!With own children of the householder under 18 years
+      23,                      # Total households!!Family households (families)!!Male householder, no wife present, family
+      24,                      # HOUSEHOLDS BY TYPE!!
+      25,                      # Total households!!Family households (families)!!Female householder, no husband present, family
+      26,                      # Total households!!Family households (families)!!Female householder, no husband present, family!!With own children of the householder under 18 years
+      27,                      # Total households!!Nonfamily households
+      28,                      # Total households!!Nonfamily households!!Householder living alone
+      29,                      # Total households!!Nonfamily households!!Householder living alone!!65 years and over
+      30,                      # Total households!!Households with one or more people under 18 years
+      31,                      # Total households!!Households with one or more people 65 years and over
+      32,                      # Total households!!Average household size
+      33,                      # Total households!!Average family size
+      34,                      # RELATIONSHIP!!Population in households
+      35,                      # RELATIONSHIP!!Population in households!!Householder
+      36,                      # RELATIONSHIP!!Population in households!!Spouse
+      37,                      # RELATIONSHIP!!Population in households!!Child
+      38,                      # RELATIONSHIP!!Population in households!!Other relatives
+      39,                      # RELATIONSHIP!!Population in households!!Nonrelatives
+      40,                      # RELATIONSHIP!!Population in households!!Nonrelatives!!Unmarried partner
+      41,                      # MARITAL STATUS!!Males 15 years and over
+      42,                      # MARITAL STATUS!!Males 15 years and over!!Never married
+      43,                      # MARITAL STATUS!!Males 15 years and over!!Now married, except separated
+      44,                      # MARITAL STATUS!!Males 15 years and over!!Separated
+      45,                      # MARITAL STATUS!!Males 15 years and over!!Widowed
+      46,                      # MARITAL STATUS!!Males 15 years and over!!Divorced
+      47,                      # MARITAL STATUS!!Females 15 years and over
+      48,                      # MARITAL STATUS!!Females 15 years and over!!Never married
+      49,                      # MARITAL STATUS!!Females 15 years and over!!Now married, except separated
+      50,                      # MARITAL STATUS!!Females 15 years and over!!Separated
+      51,                      # MARITAL STATUS!!Females 15 years and over!!Widowed
+      52,                      # MARITAL STATUS!!Females 15 years and over!!Divorced
+      53,                      # SCHOOL ENROLLMENT!!Population 3 years and over enrolled in school
+      54,                      # SCHOOL ENROLLMENT!!Population 3 years and over enrolled in school!!Nursery school, preschool
+      55,                      # SCHOOL ENROLLMENT!!Population 3 years and over enrolled in school!!Kindergarten
+      56,                      # SCHOOL ENROLLMENT!!Population 3 years and over enrolled in school!!Elementary school (grades 1-8)
+      57,                      # SCHOOL ENROLLMENT!!Population 3 years and over enrolled in school!!High school (grades 9-12)
+      58,                      # SCHOOL ENROLLMENT!!Population 3 years and over enrolled in school!!College or graduate school
+      69,                      # VETERAN STATUS!!Civilian population 18 years and over
+      70,                      # VETERAN STATUS!!Civilian population 18 years and over!!Civilian veterans
+      71,                      # DISABILITY STATUS OF THE CIVILIAN NONINSTITUTIONALIZED POPULATION!!Total Civilian Noninstitutionalized Population
+      72,                      # DISABILITY STATUS OF THE CIVILIAN NONINSTITUTIONALIZED POPULATION!!Total Civilian Noninstitutionalized Population!!With a disability
+      73,                      # DISABILITY STATUS OF THE CIVILIAN NONINSTITUTIONALIZED POPULATION!!Under 18 years
+      74,                      # DISABILITY STATUS OF THE CIVILIAN NONINSTITUTIONALIZED POPULATION!!Under 18 years!!With a disability
+      75,                      # DISABILITY STATUS OF THE CIVILIAN NONINSTITUTIONALIZED POPULATION!!18 to 64 years
+      76,                      # DISABILITY STATUS OF THE CIVILIAN NONINSTITUTIONALIZED POPULATION!!18 to 64 years!!With a disability
+      77,                      # DISABILITY STATUS OF THE CIVILIAN NONINSTITUTIONALIZED POPULATION!!65 years and over
+      78,                      # DISABILITY STATUS OF THE CIVILIAN NONINSTITUTIONALIZED POPULATION!!65 years and over!!With a disability
+      79,                      # TOT_POP
+      80,                      # TOT_MALE
+      81,                      # TOT_FEMALE
+      82,                      # WA_MALE
+      83,                      # WA_FEMALE
+      84,                      # BA_MALE
+      85,                      # BA_FEMALE
+      86,                      # IA_MALE
+      87,                      # IA_FEMALE
+      88,                      # AA_MALE
+      89,                      # AA_FEMALE
+      90,                      # NA_MALE
+      91,                      # NA_FEMALE
+      92,                      # TOM_MALE
+      93,                      # TOM_FEMALE
+      94,                      # WAC_MALE
+      95,                      # WAC_FEMALE
+      96,                      # BAC_MALE
+      97,                      # BAC_FEMALE
+      98,                      # IAC_MALE
+      99,                      # IAC_FEMALE
+      100,                      # AAC_MALE
+      101,                      # AAC_FEMALE
+      102,                      # NAC_MALE
+      103,                      # NAC_FEMALE
+      104,                      # NH_MALE
+      105,                      # NH_FEMALE
+      106,                      # NHWA_MALE
+      107,                      # NHWA_FEMALE
+      108,                      # NHBA_MALE
+      109,                      # NHBA_FEMALE
+      110,                      # NHIA_MALE
+      111,                      # NHIA_FEMALE
+      112,                      # NHAA_MALE
+      113,                      # NHAA_FEMALE
+      114,                      # NHNA_MALE
+      115,                      # NHNA_FEMALE
+      116,                      # NHTOM_MALE
+      117,                      # NHTOM_FEMALE
+      118,                      # NHWAC_MALE
+      119,                      # NHWAC_FEMALE
+      120,                      # NHBAC_MALE
+      121,                      # NHBAC_FEMALE
+      122,                      # NHIAC_MALE
+      123,                      # NHIAC_FEMALE
+      124,                      # NHAAC_MALE
+      125,                      # NHAAC_FEMALE
+      126,                      # NHNAC_MALE
+      127,                      # NHNAC_FEMALE
+      128,                      # H_MALE
+      129,                      # H_FEMALE
+      130,                      # HWA_MALE
+      131,                      # HWA_FEMALE
+      132,                      # HBA_MALE
+      133,                      # HBA_FEMALE
+      134,                      # HIA_MALE
+      135,                      # HIA_FEMALE
+      136,                      # HAA_MALE
+      137,                      # HAA_FEMALE
+      138,                      # HNA_MALE
+      139,                      # HNA_FEMALE
+      140,                      # HTOM_MALE
+      141,                      # HTOM_FEMALE
+      142,                      # HWAC_MALE
+      143,                      # HWAC_FEMALE
+      144,                      # HBAC_MALE
+      145,                      # HBAC_FEMALE
+      146,                      # HIAC_MALE
+      147,                      # HIAC_FEMALE
+      148,                      # HAAC_MALE
+      149,                      # HAAC_FEMALE
+      150,                      # HNAC_MALE
+      151                       # HNAC_FEMALE
     ])
   ])
 
@@ -248,7 +393,8 @@ class Formatter():
       'poverty': join(self.raw_data_dir, 'national', 'Socioeconomic_status', 'PovertyEstimates.csv'),
       'unemployment': join(self.raw_data_dir, 'national', 'Socioeconomic_status', 'Unemployment.csv'),
       'climate': join(self.raw_data_dir, 'national', 'Climate', 'FIPS_2019_precipitation_tempAvg_tempMin_tempMax.csv'),
-      'density': join(self.raw_data_dir, 'national', 'Density', 'housing_area_density_national_2010_census.csv')
+      'density': join(self.raw_data_dir, 'national', 'Density', 'housing_area_density_national_2010_census.csv'),
+      'demographics': join(self.raw_data_dir, 'aggregated', 'demographics_by_county.csv')
     }
     
     self._make_reference()
@@ -364,6 +510,8 @@ class Formatter():
     fips = self._get_fips(x)
     return fips[1] == '1' and fips[4] == '0'
 
+  national_data_delimiters = {'demographics': ';'}
+  
   def make_national_data(self):
     """Make the national data.
 
@@ -383,13 +531,15 @@ class Formatter():
 
     for k in self.keys:
       with open(self.national_data_filenames[k], 'r', newline='') as file:
-        reader = csv.reader(file, delimiter=',')
-        
+        delimiter = self.national_data_delimiters.get(k, ',')
+        reader = csv.reader(file, delimiter=delimiter)
         for i, row in enumerate(reader):
-          if i < self.national_data_skiprows[k]:
+          if i < self.national_data_skiprows[k]:            
             continue
 
           if i == self.national_data_skiprows[k]:
+            print(k)
+            print(*list(map(lambda t: f'      {t[0]},                      # {t[1]}', enumerate(row))), sep='\n')
             self.national_data['labels'][k] = [row[j].replace(',', '') for j in self.national_data_which_columns[k]]
             continue
 
@@ -415,13 +565,15 @@ class Formatter():
               values[j] = 'NA'
 
           self.national_data[fips][k] = values
-          
+
+    # write to the csv
     with open(join(self.data_dir, 'counties.csv'), 'w', newline='') as file:
       writer = csv.writer(file, delimiter=',')
       writer.writerow(sum([self.national_data['labels'][k] for k in self.keys], []))
 
-      for fips in self.fips_codes:
+      for i, fips in enumerate(self.fips_codes):
         writer.writerow(sum([self.national_data[fips][k] for k in self.keys], []))
+      print(f'wrote data for {i} counties')
 
   def _read_cases_data(self):
     infections_filename = join(self.raw_data_dir, 'national', )
@@ -454,11 +606,11 @@ class Formatter():
     filename = join(self.data_dir, 'beta_gamma.csv')
     file = open(filename, 'w', newline='')
     writer = csv.writer(file, delimiter=',')
-    writer.writerow(['FIPS', 'beta', 'gamma'])
+    writer.writerow(['FIPS', 'infected', 'beta', 'gamma'])
 
     for fips in self.fips_codes:
       if not (fips in infections and fips in deaths and fips in recovered):
-        writer.writerow([fips, 'NA', 'NA'])
+        writer.writerow([fips, 'NA', 'NA', 'NA'])
         continue
 
       # Total population, N.
@@ -477,7 +629,7 @@ class Formatter():
       beta = - (S[-1] - S[0]) / np.trapz(S - X, x=None, dx=1)
       gamma = R / np.trapz()
       
-      writer.writerow([fips, f'{beta}', f'{gamma}'])
+      writer.writerow([fips, f'{infections[fips][-1]}', f'{beta}', f'{gamma}'])
       print(f'wrote {fips}: beta = {beta:.04f}, gamma = {gamma:.04f}')
     file.close()
 
